fff4e3313b6ebae91b716d88289055375eb3fb3e
Akram Noui <im_noui@esi.dz>
2020-06-17 00:39:01 +0100

    finalizing project

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -62,6 +62,12 @@ class App extends React.Component {
   savePlaylist(){
     const trackURIs = this.state.playlistName.map(track =>
       track.uri)
+      Spotify.savePlaylist(this.state.playlistName, trackURIs).then(() => {
+        this.setState({
+          playlistName: "New Playlist",
+          playlistTracks: []
+        });
+      });
   }
 
   search(term){
