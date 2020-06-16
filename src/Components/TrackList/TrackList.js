c232062b327cb829a92bb4ad7c56ef521f81427f
Akram Noui <im_noui@esi.dz>
2020-06-16 21:07:10 +0100

    finalizing add method

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,6 +1,7 @@
 import React from "react"
-import " ./TrackList.css";
 import Track from "../Track/Track";
+import "./TrackList.css";
+
 
 
 
@@ -14,7 +15,7 @@ class TrackList extends React.Component{
                 <div className="TrackList">
                 {
                     this.props.tracks.map(track => {
-                        return ( <Track track={track} key={track.id} onAdd={this.props.onAdd} />);
+                        return ( <Track track={track} key={track.id} onAdd={this.props.onAdd} isRemoval={this.props.isRemoval} />);
 
                     })
                 }
