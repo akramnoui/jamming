e79404e874dd0eb6671f0b6d71e5843ae16ace71
Akram Noui <im_noui@esi.dz>
2020-06-17 00:36:47 +0100

    get playlist ID

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,8 +1,6 @@
 
 
-const accessToken=false;
-var xhr = new XMLHttpRequest()
-
+let accessToken;
 const clientId = 'dad489bb55cd4a318db053581b6485bd';
 const redirectUri = "http://localhost:3000/";
 
@@ -57,7 +55,40 @@ const Spotify ={
 
         });
 
-        }
+        
+    } , 
+    savePlaylist(name  , trackURIs){
+        if (!name || !trackURIs.length) {
+            return;
+          }
+          const accessToken = Spotify.getAccessToken();
+          const headers = { Authorization: `Bearer ${accessToken}` };
+          let userId;
+          return fetch(`https://api.spotify.com/v1/me` , { headers: headers }).then( response  => {
+              response.json();
+          }).then(jsonResponse => {
+              userId = jsonResponse.id;
+              return fetch(`https://api.spotify.com/v1/users/${userId}/playlists` ,{
+                 headers: headers , 
+                 method: "POST" , 
+                 body: JSON.stringify({name: name}) 
+              } ).then(response => {
+                  response.json()
+              }).then(jsonResponse =>{
+                  const playlistID = jsonResponse.id;
+                  return fetch(
+                    `https://api.spotify.com/v1/users/${userId}/playlists/${playlistID}/tracks`,
+                    {
+                      headers: headers,
+                      method: "POST",
+                      body: JSON.stringify({ uris: trackURIs })
+                    }
+                  );
+       
+              })
+          })
+      
+
     }
 
 }
