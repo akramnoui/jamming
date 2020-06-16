9867e7a1f29dd9be50b57e0c20ecebd1931ba257
Akram Noui <im_noui@esi.dz>
2020-06-17 00:02:51 +0100

    adding spotify client ID

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,14 +1,62 @@
 
 
-const accessToken="";
+const accessToken=false;
+var xhr = new XMLHttpRequest()
+
+const clientId = 'dad489bb55cd4a318db053581b6485bd';
+const redirectUri = "http://localhost:3000/";
 
 
-   const  getAccessToken= () =>{
-        if(accessToken != ""){
+const Spotify ={
+
+    
+
+    getAccessToken(){
+        if(accessToken){
             return accessToken;
+        }
+
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if(accessTokenMatch && expiresInMatch){
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            
+        window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
+        window.history.pushState("Access Token", null, "/");
+        return accessToken;
+
+        }else{
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            window.location = accessUrl;
+
+        }
+
+    } , 
+    search(term){
+        const accessToken = this.getAccessToken();
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}` , {
+            headers: {Authorization: `Bearer ${accessToken}`}
+
+        }).then( response =>{
+            response.json();
+        }).then(jsonResponse =>{
+            if(!jsonResponse){
+                return [];
+            }
+            
+
+
+
+        })
+
         }
     }
 
+}
+
+ 
 
 
 
