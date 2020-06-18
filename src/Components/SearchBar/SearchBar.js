9e0b8d3b5abe3aec0ab9b8da2557bbf46258aaae
Akram Noui <im_noui@esi.dz>
2020-06-18 01:39:37 +0100

    adding nav bar & refresh token

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -30,7 +30,7 @@ class SearchBar extends React.Component{
             return(
                 <div className="SearchBar">
                 <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange}/>
-                <button className="SearchButton">SEARCH</button>
+                <button className="SearchButton" onClick={this.Search}>SEARCH</button>
               </div>
             );
 
