27c41d4a57fb3136ed4bf74b7bad8ffe9e6e4fae
Akram Noui <im_noui@esi.dz>
2020-10-27 23:58:08 +0100

    Updates

---
+++ precache-manifest.cf38793808c0350f8eb3443502c19eba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0aea1e374206116b1cbd8f0106ae6a9",
+    "url": "/jamming/index.html"
+  },
+  {
+    "revision": "1a52dac37c237754ef8b",
+    "url": "/jamming/static/css/main.c12e4f97.chunk.css"
+  },
+  {
+    "revision": "84efed936aee17242b3a",
+    "url": "/jamming/static/js/2.51f7259a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jamming/static/js/2.51f7259a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a52dac37c237754ef8b",
+    "url": "/jamming/static/js/main.faf99330.chunk.js"
+  },
+  {
+    "revision": "791716a19457ad6a4952",
+    "url": "/jamming/static/js/runtime-main.b4c91255.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jamming/static/media/background_photo_desktop.37be7357.jpg"
+  },
+  {
+    "revision": "1b324c0257bcc1f89757ed8cb5eccb4a",
+    "url": "/jamming/static/media/profile.1b324c02.png"
+  }
+]);
