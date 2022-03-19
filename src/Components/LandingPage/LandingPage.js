43988e7ef1191e8a422163fe9897160c563bfc42
Akram Noui <im_noui@esi.dz>
2022-03-19 19:31:40 +0100

    adding Readme

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -11,9 +11,8 @@ import { Link } from "react-router-dom";
         <div className="login" id="DIV_1">
             <div className="login__content" id="DIV_4">
                 <h1 className="login__title" id="H1_5">Make Your Spotify Playlist</h1>
-               <h3 className="h3-description">We are looking to create a 1 minute video ad for a new online shopping game. Our desired video was 
-                   inspired by this video in the link below. 
-                   We are looking to get a similar and even BETTER affect with our video, 
+               <h3 className="h3-description">Playlist Making made easy ! With this App you can create your custom playlosts effortlessly with only on click
+               , it only require you to Login with your Spotify Account and you're all set. More feature coming along !
                 </h3>
                 <Link to={"/App"}>
                 <button className="btn btn-primary btn-login" id="BUTTON_6">Sign in with Spotify</button>
--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,70 +1,70 @@
-import React , {useState , useEffect} from 'react'
-import './MainPage.css';
+// import React , {useState , useEffect} from 'react'
+// import './MainPage.css';
 
-import Playlist from '../Playlist/Playlist';
+// import Playlist from '../Playlist/Playlist';
 
-import SearchBar from '../SearchBar/SearchBar';
-import SearchResults from '../SearchResults/SearchResults';
-
-
-
-export default function MainPage() {
-
-    
+// import SearchBar from '../SearchBar/SearchBar';
+// import SearchResults from '../SearchResults/SearchResults';
 
 
-    useEffect(()=> {
 
-    })
-  return (
-    <div>
-    <header>
-       <nav class="navbar navbar-expand-lg navbar-dark "  >
-         <div className='navbar-brand'>
-          <div className="LogoContainer">
-                  <img className="logo" src={require('./audio.png')} />
+// export default function MainPage() {
 
-                  <h1 className="akramspotify">Akram <span className="highlight">Spotify</span></h1>
 
-                </div> 
-          </div>  
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
-            </li>
-          </ul>
-          <div className='navbar-text'>
+
+
+//     useEffect(()=> {
+
+//     })
+//   return (
+//     <div>
+//     <header>
+//        <nav class="navbar navbar-expand-lg navbar-dark "  >
+//          <div className='navbar-brand'>
+//           <div className="LogoContainer">
+//                   <img className="logo" src={require('./audio.png')} />
+
+//                   <h1 className="akramspotify">Akram <span className="highlight">Spotify</span></h1>
+
+//                 </div> 
+//           </div>  
+//         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+//           <span class="navbar-toggler-icon"></span>
+//         </button>
+//         <div class="collapse navbar-collapse" id="navbarText">
+//           <ul class="navbar-nav mr-auto">
+//             <li class="nav-item active">
+//               <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+//             </li>
+//             <li class="nav-item">
+//               <a class="nav-link" href="#">Features</a>
+//             </li>
+//             <li class="nav-item">
+//               <a class="nav-link" href="#">Pricing</a>
+//             </li>
+//           </ul>
+//           <div className='navbar-text'>
             
-            <div className="flex-container">
-              <h2 className="pname">{this.state.userId}</h2>
-              <img className="pp" src={this.state.imgurl} />
-            </div>
-          </div> 
-        </div>
-      </nav>
+//             <div className="flex-container">
+//               <h2 className="pname">{this.state.userId}</h2>
+//               <img className="pp" src={this.state.imgurl} />
+//             </div>
+//           </div> 
+//         </div>
+//       </nav>
   
 
 
     
-    </header>
+//     </header>
            
-     <div className="App">
-       <SearchBar onSearch={this.search}/>
-         <div className="App-playlist">
-           <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}  />
-           <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist}/>
-         </div>
-     </div>
-   </div>
-  )
-}
+//      <div className="App">
+//        <SearchBar onSearch={this.search}/>
+//          <div className="App-playlist">
+//            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}  />
+//            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist}/>
+//          </div>
+//      </div>
+//    </div>
+//   )
+// }
